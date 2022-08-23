3056d0d925723ef105b170e641f0032e626837a3
Pramila <pramilamanickavasakan@gmail.com>
2022-08-23 19:45:48 +0530

    initial commit

---
+++ aod1.py
@@ -0,0 +1,383 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  1 14:17:02 2022
+
+@author: PRAMILA
+"""
+from __future__ import division # ensures no rounding errors from division involving integers
+from math import * # enables use of pi, trig functions, and more.
+
+import pandas as pd # gives us the dataframe concept
+pd.options.display.max_columns = 50
+pd.options.display.max_rows = 9
+import streamlit as st
+from pyhdf.SD import SD, SDC
+
+from mpl_toolkits.basemap import Basemap
+
+import numpy as np
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pylab
+import h5py
+import datetime
+from urllib.request import urlopen
+import urllib.request
+import urllib.request, json
+import fnmatch
+import re
+
+from urllib.request import urlopen
+opener = urllib.request.build_opener()
+opener.addheaders = [('Authorization', 'Bearer cHJhbWlsYV9tYW5pY2thdmFzYWthbjpjSEpoYldsc1lTNHhPVEF4TVRNM1FITnlaV011WVdNdWFXND06MTY2MTI1MTQxNDo4NTJjMWVhYzQyMWExMTNmOGI0ZGI4M2UxNTEwMzRiZTM3ZWZlNzlk')]
+urllib.request.install_opener(opener)
+
+user_lat=st.text_input('\nPlease enter the latitude you would like to analyze (Deg. N): ')
+user_lon=st.text_input('Please enter the longitude you would like to analyze (Deg. E): ')
+plantsize_type= st.radio("Choose any one of Solar Panel Capacity you want to install",('Individual home/flat','Residential Flat','Commerical Industry'))
+if(plantsize_type=="Individual home/flat"):
+    plantsize=1
+elif(plantsize_type=="Residential Flat"):
+    plantsize=5    
+else:
+    plantsize=50      
+today = datetime.date.today()-datetime.timedelta(days=1)
+
+date = st.date_input('📅 Date', value = today,max_value=today)
+date=date.strftime("%Y/%m/%d")
+fmt = '%Y/%m/%d'
+dt = datetime.datetime.strptime(date, fmt)
+tt = dt.timetuple()
+julian_day=tt.tm_yday
+st.write(julian_day)
+df = pd.read_csv(r"C:\Users\PRAMILA\Downloads\world-cities.csv")
+list1 = list(np.unique(df["name"].values))
+list2 = list(np.unique(df["country"].values))
+list3 = list(np.unique(df["subcountry"].astype(str).values))
+locations = list1 + list2 + list3
+
+
+location_selector = st.selectbox("Select a Location",locations)
+
+if st.button("Predict"):
+    user_lat=float(user_lat)
+    user_lon=float(user_lon)
+    urlstr='https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MOD04_L2/2022/{}.json'.format(julian_day)
+    st.write(urlstr)
+    urlpath =urlopen(urlstr)
+    print(urlpath)
+    data = json.loads(urlpath.read())
+    print(data)
+    print("***************************************************************")
+    
+    file_list = list(map(lambda x : x['name'], data))
+    print(f'Print Title --> {file_list}')
+    print(len(file_list))
+    i=0
+    import os
+    directory = str(julian_day)
+    parent_dir = "C:/Users/PRAMILA/.spyder-py3/project/sih/data/L3/"
+    path = os.path.join(parent_dir, directory)
+    st.write(path)
+    if not os.path.exists(path):
+        os.mkdir(path)
+    else:
+        
+        if(len(fnmatch.filter(os.listdir(path), '*.*'))==155):
+            pass
+        else:
+            with st.spinner("Downloading the data...."):
+                for file_name in file_list:
+                    print("downloading {}".format(i))                
+                    ladsweb_url = 'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MOD04_L2/2022/{}/{}'.format(julian_day,file_name)
+                    target_dir = path+"/"+file_name
+                    
+                    urllib.request.urlretrieve(ladsweb_url,target_dir)
+                    i=i+1
+    
+   # file_name=r"C:\Users\PRAMILA\.spyder-py3\project\sih\data\L3\MOD04_L2.A2022218.0000.061.2022218132236.hdf"
+    
+    datetime=[]
+    aod_value=[]
+    columns=['DATETIME','AOD']
+
+    for root, dirs, files in os.walk(path+"/"):
+        for file in files:
+            if os.path.splitext(file)[1] == '.hdf':
+                filePath = os.path.join(root, file)
+    
+                hdf= SD(filePath, SDC.READ)
+                sds=hdf.select('AOD_550_Dark_Target_Deep_Blue_Combined')
+                data=sds.get()
+    
+    
+                # Get lat and lon info
+                lat = hdf.select('Latitude')
+                latitude = lat[:,:]
+                print("latitude: ",latitude)
+                
+                min_lat=latitude.min()
+                max_lat=latitude.max()
+                print("min_lat: ",min_lat)
+                print("max_lat: ",max_lat)
+                
+                lon = hdf.select('Longitude')
+                longitude = lon[:,:]
+                min_lon=longitude.min()
+                max_lon=longitude.max()
+                
+                attributes=sds.attributes()
+                scale_factor=attributes['scale_factor']
+                print("scale_factor  ",scale_factor)
+                fillvalue=attributes['_FillValue']
+                
+                user_lat=float(user_lat)
+                user_lon=float(user_lon)
+                #calculation to find nearest point in data to entered location (haversine formula)
+                R=6371000#radius of the earth in meters
+                
+                #degree to radians
+                
+                lat1=np.radians(user_lat)
+                print("lat1: ",lat1)
+                
+                lat2=np.radians(latitude)
+                print("lat2: ",lat2)
+                
+                delta_lat=np.radians(latitude-user_lat)
+                print("delta_lat: ",delta_lat)
+                
+                delta_lon=np.radians(longitude-user_lon)
+                
+                a=(np.sin(delta_lat/2))*(np.sin(delta_lat/2))+(np.cos(lat1))*(np.cos(lat2))*(np.sin(delta_lon/2))*(np.sin(delta_lon/2))
+                print("a : ",a)
+                c=2*np.arctan2(np.sqrt(a),np.sqrt(1-a))
+                print("c : ",c)
+                d=R*c
+                print("d: ",d)
+                #gets (and then prints) the x,y location of the nearest point in data to entered location, accounting for no data values
+                x,y=np.unravel_index(d.argmin(),d.shape)
+                print("x ",x,"y ", y)
+                print('\nThe nearest pixel to your entered location is at: \nLatitude:',latitude[x,y],' Longitude:',longitude[x,y])
+    
+    
+    
+                if data[x,y]==fillvalue:
+                     	print('The value of AOD at this pixel is',fillvalue,',(No Value)\n')
+                     	AOD500nm=0
+                else:
+                     	print('The value of AOD at this pixel is ',round(data[x,y]*scale_factor,3))
+                     	AOD500nm=round(data[x,y]*scale_factor,3)
+                aod_value.append(AOD500nm)
+                
+                date = re.compile("[0-9]{13}")
+                date = date.search(filePath)
+                date=date.group(0)
+                date = rf'{date[4:7]}-{date[0:4]}-{date[7:9]}:{date[9:11]}:{date[11:13]}'
+                print(date)
+                
+                datetime.append(date)
+                
+                
+                
+    df = pd.DataFrame(list(zip(datetime,aod_value)), columns=columns)
+    print(df)
+    df.to_csv(r"C:\Users\PRAMILA\.spyder-py3\project\sih\datas.csv")
+    
+    ### User Inputs
+    AOD500nm=np.mean(aod_value)
+    st.success("AOD Value {} ".format(round(AOD500nm,3)))
+    phi = user_lat
+    longitude = user_lon
+    tz = -7
+    P_mb = 840
+    Ozone_cm = 0.3
+    H20_cm = 1.5
+    
+    AOD380nm = 0.15
+    Taua = (0.2758*AOD380nm)+(0.35*AOD500nm)
+    Ba = 0.85
+    albedo = 0.2
+    
+    G_sc = 1367 # W/m^2
+    std_mer = longitude-longitude%15+15 # This Standard Meridian calculation is only a guide!! 
+                                        # Please double check this value for your location!
+    
+    ### Day of the Year Column
+    
+    n = 218 # julian day of the year
+    n_hrly = list(pd.Series(n).repeat(24)) # julian day numbers repeated hourly to create 8760 datapoints in dataset
+    
+    ds = pd.DataFrame(n_hrly, columns=['DOY']) # create dataframe with julian days 
+    
+    ### Hr of the Day Column
+    
+    ds['HR'] = [(hr)%24 for hr in ds.index.tolist()] # append dataframe with hr of the day for each day
+    
+    ### Extraterrestrial Radiation
+    
+    def etr(n):
+        return G_sc*(1.00011+0.034221*cos(2*pi*(n-1)/365)+0.00128*sin(2*pi*(n-1)/365)+0.000719*cos(2*(2*pi*(n-1)/365))+0.000077*sin(2*(2*pi*(n-1)/365)))
+    
+    ds['ETR'] = [etr(n) for n in ds['DOY']] # append dataframe with etr for day
+    
+    ### Intermediate Parameters
+    
+    ds['Dangle'] = [2*pi*(n-1)/365 for n in ds['DOY']]
+
+    def decl(Dangle):
+        return (0.006918-0.399912*cos(Dangle)+0.070257*sin(Dangle)-0.006758*cos(2*Dangle)+0.000907*sin(2*Dangle)-0.002697*cos(3*Dangle)+0.00148*sin(3*Dangle))*(180/pi)
+    ds['DEC'] = [decl(Dangle) for Dangle in ds['Dangle']]
+    
+    def eqtime(Dangle):
+        return (0.0000075+0.001868*cos(Dangle)-0.032077*sin(Dangle)-0.014615*cos(2*Dangle)-0.040849*sin(2*Dangle))*229.18
+    ds['EQT'] = [eqtime(Dangle) for Dangle in ds['Dangle']]
+    
+    def omega(hr, eqt):
+        return 15*(hr-12.5) + longitude - tz*15 + eqt/4
+    ds['Hour Angle'] = [omega(hr, eqt) for hr, eqt in zip(ds['HR'],ds['EQT'])]
+    
+    def zen(dec, hr_ang):
+        return acos(cos(dec/(180/pi))*cos(phi/(180/pi))*cos(hr_ang/(180/pi))+sin(dec/(180/pi))*sin(phi/(180/pi)))*(180/pi)
+    ds['Zenith Ang'] = [zen(dec, hr_ang) for dec, hr_ang in zip(ds['DEC'],ds['Hour Angle'])]
+    
+    def airmass(zenang):
+        if zenang < 89:
+            return 1/(cos(zenang/(180/pi))+0.15/(93.885-zenang)**1.25)
+        else:
+            return 0
+    ds['Air Mass'] = [airmass(zenang) for zenang in ds['Zenith Ang']]
+    
+    ### Intermediate Results
+    
+    def T_rayleigh(airmass):
+        if airmass > 0:
+            return exp(-0.0903*(P_mb*airmass/1013)**0.84*(1+P_mb*airmass/1013-(P_mb*airmass/1013)**1.01))
+        else:
+            return 0
+    ds['T rayleigh'] = [T_rayleigh(airmass) for airmass in ds['Air Mass']]
+    
+    def T_ozone(airmass):
+        if airmass > 0:
+            return 1-0.1611*(Ozone_cm*airmass)*(1+139.48*(Ozone_cm*airmass))**-0.3034-0.002715*(Ozone_cm*airmass)/(1+0.044*(Ozone_cm*airmass)+0.0003*(Ozone_cm*airmass)**2)
+        else:
+            return 0
+    ds['T ozone'] = [T_ozone(airmass) for airmass in ds['Air Mass']]
+    
+    def T_gasses(airmass):
+        if airmass > 0:
+            return exp(-0.0127*(airmass*P_mb/1013)**0.26)
+        else:
+            return 0
+    ds['T gases'] = [T_gasses(airmass) for airmass in ds['Air Mass']]
+    
+    def T_water(airmass):
+        if airmass > 0:
+            return 1-2.4959*airmass*H20_cm/((1+79.034*H20_cm*airmass)**0.6828+6.385*H20_cm*airmass)
+        else:
+            return 0
+    ds['T water'] = [T_water(airmass) for airmass in ds['Air Mass']]
+    
+    def T_aerosol(airmass):
+        if airmass > 0:
+            return exp(-(Taua**0.873)*(1+Taua-Taua**0.7088)*airmass**0.9108)
+        else:
+            return 0
+    ds['T aerosol'] = [T_aerosol(airmass) for airmass in ds['Air Mass']]
+    
+    def taa(airmass, taerosol):
+        if airmass > 0:
+            return 1-0.1*(1-airmass+airmass**1.06)*(1-taerosol)
+        else:
+            return 0
+    ds['TAA'] = [taa(airmass, taerosol) for airmass, taerosol in zip(ds['Air Mass'],ds['T aerosol'])]
+    
+    def rs(airmass, taerosol, taa):
+        if airmass > 0:
+            return 0.0685+(1-Ba)*(1-taerosol/taa)
+        else:
+            return 0
+    ds['rs'] = [rs(airmass, taerosol, taa) for airmass, taerosol, taa in zip(ds['Air Mass'],ds['T aerosol'],ds['TAA'])]
+    
+    def Id(airmass, etr, taerosol, twater, tgases, tozone, trayleigh):
+        if airmass > 0:
+            return 0.9662*etr*taerosol*twater*tgases*tozone*trayleigh
+        else:
+            return 0
+    ds['Id'] = [Id(airmass, etr, taerosol, twater, tgases, tozone, trayleigh) for airmass, etr, taerosol, twater, tgases, tozone, trayleigh in zip(ds['Air Mass'],ds['ETR'],ds['T aerosol'],ds['T water'],ds['T gases'],ds['T ozone'],ds['T rayleigh'])]
+    
+    def idnh(zenang, Id):
+        if zenang < 90:
+            return Id*cos(zenang/(180/pi))
+        else:
+            return 0
+    ds['IdnH'] = [idnh(zenang, Id) for zenang, Id in zip(ds['Zenith Ang'],ds['Id'])]
+    
+    def ias(airmass, etr, zenang, tozone, tgases, twater, taa, trayleigh, taerosol):
+        if airmass > 0:
+            return etr*cos(zenang/(180/pi))*0.79*tozone*tgases*twater*taa*(0.5*(1-trayleigh)+Ba*(1-(taerosol/taa)))/(1-airmass+(airmass)**1.02)
+        else:
+            return 0
+    ds['Ias'] = [ias(airmass, etr, zenang, tozone, tgases, twater, taa, trayleigh, taerosol) for airmass, etr, zenang, tozone, tgases, twater, taa, trayleigh, taerosol in zip(ds['Air Mass'],ds['ETR'],ds['Zenith Ang'],ds['T ozone'],ds['T gases'],ds['T water'],ds['TAA'],ds['T rayleigh'],ds['T aerosol'])]
+    
+    def gh(airmass, idnh, ias, rs):
+        if airmass > 0:
+            return (idnh+ias)/(1-albedo*rs)
+        else:
+            return 0
+    ds['GH'] = [gh(airmass, idnh, ias, rs) for airmass, idnh, ias, rs in zip(ds['Air Mass'],ds['IdnH'],ds['Ias'],ds['rs'])]
+    
+    ### Decimal Time
+    
+    def dectime(doy, hr):
+        return doy+(hr-0.5)/24
+    ds['Decimal Time'] = [dectime(doy, hr) for doy, hr in zip(ds['DOY'],ds['HR'])]
+    
+    ### Model Results (W/m^2)
+    
+    ds['Direct Beam'] = ds['Id']
+    
+    ds['Direct Hz'] = ds['IdnH']
+    
+    ds['Global Hz'] = ds['GH']
+    
+    ds['Dif Hz'] = ds['Global Hz']-ds['Direct Hz']
+    
+    ds[11:15]
+    
+    st.write(ds)
+    pylab.rcParams['figure.figsize'] = 16, 6  # this sets the default image size for this session
+    
+    ax = ds[ds['DOY']==212].plot('HR',['Global Hz','Direct Hz','Dif Hz'],title='Bird Clear Sky Model Results')
+    ax.set_xlabel("Hour")
+    ax.set_ylabel("Irradiance W/m^2")
+    majorx = ax.set_xticks(range(0,25,1))
+    majory = ax.set_yticks(range(0,1001,200))
+    
+    #min_index=ds['Direct Beam'].argmin()
+    #print(min_index)
+    #st.write(ds['Direct Beam'].mean())
+    diffuse_solar_irradiance=ds['Dif Hz'].mean()
+    st.write("diffuse_solar_irradiance {} W/m2".format(diffuse_solar_irradiance))
+    solar_irradiance=ds['ETR'].mean()-diffuse_solar_irradiance
+    
+    #plantsize=2#46kw
+    total_electricity=solar_irradiance*0.0036*1.1*plantsize*300
+    #savings
+    electricity_per_unit=8#8/kwh
+    
+    monthly_savings=(total_electricity/12)*electricity_per_unit
+    annual_savings=(total_electricity)*electricity_per_unit
+    
+
+    st.success("Total electricity from solar plant(Annually) {} kW".format(round(solar_irradiance,2)))
+    st.write("1 Unit Cost:₹8")
+    st.info("FINANCIAL SAVINGS")
+    st.markdown(f"""<h1 style='text-align: left; font-weight:bold;color:black;background-color:yellow;font-size:11pt;'>Monthly savings: ₹ <mark style="background-color:white">{format(round(monthly_savings,0))} </mark> </h1>""",unsafe_allow_html=True)
+    #st.warning("Electricity Cost per Day: ₹ {}".format(round((solar_irradiance*9*24)/1000,2)))
+    st.markdown(f"""<h1 style='text-align: left; font-weight:bold;color:black;background-color:pink;font-size:11pt;'>Annual savings: ₹ <mark style="background-color:white">{format(round(annual_savings,0))}</mark> </h1>""",unsafe_allow_html=True)
+    #st.info("Electricity Cost per Month: ₹ {}".format(round((solar_irradiance*9*720)/1000,2)))
+	           
+
+    
